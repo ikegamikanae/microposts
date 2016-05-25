dc6ed909e84b86a6a35fac86e2f9e6dcbabdce6b
Kanae <fujii_kanae@hotmail.com>
2016-05-25 03:37:14 +0000

    reset and migrate db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524021551) do
 ActiveRecord::Schema.define(version: 20160524150953) do
 
   create_table "microposts", force: :cascade do |t|
